a531648b6609eed1e4d5ceccf1c15eda17ad0194
brandoneprice31 <bprice@college.harvard.edu>
2017-12-18 17:14:24 -0500

    added python server

---
+++ build/main.py
@@ -0,0 +1,32 @@
+from sanic import Sanic
+from sanic.response import html, redirect
+from sanic.exceptions import NotFound
+from validator.validator import is_mobile, is_http
+from template_loader.template_loader import template
+
+app = Sanic()
+
+# static files
+app.static('/static', './static')
+
+# middlewares
+@app.middleware('request')
+async def redirect_host_urls(request):
+    if 'localhost' in request.url:
+        return None
+    if is_http(request):
+        return redirect('https://' + request.url[len('http://'):])
+
+# exceptions
+@app.exception(NotFound)
+async def notfound(request, exception):
+    return redirect('/')
+
+# endpoints
+@app.route("/")
+async def index(request):
+    return html(template('index.html').render())
+
+if __name__ == "__main__":
+    print("Starting up newsroom-web...")
+    app.run(host="0.0.0.0", port=8081)
