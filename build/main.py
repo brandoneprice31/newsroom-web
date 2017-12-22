6fe8ff02736bcb9f3b198c985f5b9bb205b0290e
brandoneprice31 <bprice@college.harvard.edu>
2017-12-22 14:16:29 -0800

    changed redirection to http instead of https

--- build/main.py
+++ build/main.py
@@ -1,13 +1,21 @@
 from sanic import Sanic
 from sanic.response import html, redirect
 from sanic.exceptions import NotFound
-from validator.validator import is_mobile, is_http
+from validator.validator import is_mobile, is_https
 from template_loader.template_loader import template
 
 app = Sanic()
 
 # static files
 app.static('/static', './static')
+
+# middlewares
+@app.middleware('request')
+async def redirect_host_urls(request):
+    if 'localhost' in request.url:
+        return None
+    if is_https(request):
+        return redirect(request.url)
 
 # endpoints
 @app.route("/")
--- build/validator/validator.py
+++ build/validator/validator.py
@@ -5,5 +5,5 @@ def is_mobile(request):
     MOBILE_AGENT_RE=re.compile(r".*(iphone|mobile|androidtouch)",re.IGNORECASE)
     return MOBILE_AGENT_RE.match(request.headers['user-agent'])
 
-def is_http(request):
-    return 'X-Forwarded-Proto' in request.headers and 'http' == request.headers['X-Forwarded-Proto']
+def is_https(request):
+    return 'X-Forwarded-Proto' in request.headers and 'https' == request.headers['X-Forwarded-Proto']
